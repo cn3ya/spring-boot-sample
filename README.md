0faa925594e300afaab2254e72a2321b97c2e70e
cn3ya <cn3ya@4voxel.com>
2021-08-05 13:06:24 +0000

    api

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # spring-boot-sample
 
-## download from spring initializr
+## spring initializr
 ```
+# download from web
 wget 'https://start.spring.io/starter.zip?type=gradle-project&language=java&bootVersion=2.5.3&baseDir=sample&groupId=com.example&artifactId=sample&name=sample&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.sample&packaging=jar&javaVersion=11&dependencies=web,lombok,mybatis,validation,quartz,actuator' -O sample-project.zip
 
 unzip sample-project.zip
+
+# use vscode extension
+
 ```
 
 ## run
 ```
 cd sample
 ./gradlew bootRun
+
+# hot reload
+./gradlew build --continuous
+./gradlew bootRun
 ```
+
+## vscode extension
++ Language Support for Java(TM) by Red Hat
---
+++ sample/src/main/java/com/example/sample/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.sample.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.sample.dto.req.Greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, Your Name is %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping(value="/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
