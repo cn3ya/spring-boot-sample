31b371225372adf4ca61a051b55b7b28d70c079f
cn3ya <cn3ya@4voxel.com>
2022-09-03 09:11:24 +0000

    redict improve

--- sample/src/main/java/com/example/sample/controller/ErrorController.java
+++ sample/src/main/java/com/example/sample/controller/ErrorController.java
@@ -1,14 +1,30 @@
 package com.example.sample.controller;
 
-import com.example.sample.dto.res.Response;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import com.example.sample.dto.res.Response;
+
+import lombok.SneakyThrows;
+
 
 @RestController
+@ControllerAdvice
 public class ErrorController {
 
     @GetMapping(value="/error_test")
     public Response errorTest() {
         throw new RuntimeException("error test");
+    }
+
+    @SneakyThrows
+    @ExceptionHandler(value=NoHandlerFoundException.class)
+    public void handleException(HttpServletResponse response) {
+        response.sendRedirect("/swagger-ui.html");
     }
 }
--- sample/src/main/java/com/example/sample/controller/ParamController.java
+++ sample/src/main/java/com/example/sample/controller/ParamController.java
@@ -1,14 +1,19 @@
 package com.example.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sample.config.AppProperties;
 import com.example.sample.dto.req.GetReq;
 import com.example.sample.dto.res.Response;
-import com.example.sample.config.AppProperties;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
-@RequestMapping("/param")
+@RequestMapping("/api/param")
 public class ParamController {
 
 	@Autowired
